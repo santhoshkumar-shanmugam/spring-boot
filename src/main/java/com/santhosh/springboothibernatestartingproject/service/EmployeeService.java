3bbdc4944d15afcd129ac2bb79b3000eb5ed4b82
santhoshkumar-shanmugam <santhoshkumar.engr@gmail.com>
2020-09-12 14:42:09 +0530

    Removed unused import

--- src/main/java/com/santhosh/springboothibernatestartingproject/service/EmployeeService.java
+++ src/main/java/com/santhosh/springboothibernatestartingproject/service/EmployeeService.java
@@ -3,8 +3,6 @@ package com.santhosh.springboothibernatestartingproject.service;
 import java.util.List;
 
 import com.santhosh.springboothibernatestartingproject.model.Employee;
-import com.santhosh.springboothibernatestartingproject.model.Role;
-import com.santhosh.springboothibernatestartingproject.transfer.EmployeeDTO;
 
 public interface EmployeeService {
 	List<Employee> get();
